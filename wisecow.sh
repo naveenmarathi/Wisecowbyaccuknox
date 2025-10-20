76de1a95ceb17c1f43a5d9e4135048e30a35a335
Naveen Marathi <158243661+naveenmarathi@users.noreply.github.com>
2025-10-20 14:56:10 +0530

    Update wisecow.sh

--- wisecow.sh
+++ wisecow.sh
@@ -24,7 +24,7 @@ Content-Type: text/html
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Kubernetes Knowledge Center</title>
+    <title>Deployment of wisecow application using Kubernetes EKS </title>
     <style>
         body {
             font-family: Arial, sans-serif;
